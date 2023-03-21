120d5f3eb6607f62aab05123f66d522c8e21fed4
Modbilas <238.juanjen@gmail.com>
2023-03-21 20:36:24 +0700

    footer

--- Siswa.php
+++ Siswa.php
@@ -39,7 +39,71 @@
         </nav>
     </head>
     <body>
-        
+    <div class="sticky-bottom">
+        <footer
+                class="text-center text-lg-start text-white"
+                style="background-color: #929fba"
+                >
+            <div class="container p-4 pb-0">
+            <section class="">
+                <div class="row">
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h6 class="text-uppercase mb-4 font-weight-bold">
+                    Company name
+                    </h6>
+                    <p>
+                    Here you can use rows and columns to organize your footer
+                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                    elit.
+                    </p>
+                </div>
+
+                <hr class="w-100 clearfix d-md-none" />
+
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+                    <p>
+                    <a class="text-white">MDBootstrap</a>
+                    </p>
+                    <p>
+                    <a class="text-white">MDWordPress</a>
+                    </p>
+                    <p>
+                    <a class="text-white">BrandFlow</a>
+                    </p>
+                    <p>
+                    <a class="text-white">Bootstrap Angular</a>
+                    </p>
+                </div>
+
+
+                <hr class="w-100 clearfix d-md-none" />
+
+                <hr class="w-100 clearfix d-md-none" />
+
+
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                    <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+                    <p><i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
+                    <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+                    <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                </div>
+
+            </section>
+            </div>
+
+            <div
+                class="text-center p-3"
+                style="background-color: rgba(0, 0, 0, 0.2)"
+                >
+            © 2020 Copyright:
+            <a class="text-white" href="https://mdbootstrap.com/"
+                >MDBootstrap.com</a
+                >
+            </div>
+        </footer>
+        </div>
     </body>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
