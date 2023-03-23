642aa0181562d6d2055ef304e40672d8cf33a5c9
David Ongky <da.ongky88@gmail.com>
2023-03-23 07:59:06 +0700

    form css

--- loginAdmin.php
+++ loginAdmin.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title>Login Admin</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     </head>
     <body>
     <div class="container">
@@ -13,10 +14,6 @@
                 <div class="form-group">
                     <label for="age">Password</label>
                     <input type="password" class="form-control" id="age" name="password">
-                </div>
-                <div>
-                    <a href="formLupaPassword.php">Lupa Password</a> atau 
-                    <a href="formSignUp.php">Sign Up</a>
                 </div>
                 <button id="Login" type="submit" class="btn btn-primary">Login</button>
             </form>
