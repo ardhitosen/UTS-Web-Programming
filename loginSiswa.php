2450782c57432d36614fa497b4061c6907f3aaf4
ardhitosen <christoforusardhito@gmail.com>
2023-03-27 17:29:57 +0700

    Update loginSiswa.php

--- loginSiswa.php
+++ loginSiswa.php
@@ -2,30 +2,30 @@
     //require_once __DIR__.'/vendor/autoload.php';
 ?>
 
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Form Siswa</title>
-        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.4/dist/tailwind.min.css" rel="stylesheet">
-    </head>
-    <body class="bg-gray-100 flex items-center justify-center h-screen">
-        <div class="max-w-md w-full space-y-8">
-            <form method="post" action="proses_login.php" class="bg-white p-6 rounded-lg shadow-md">
-                <div class="mb-6">
-                    <label for="email" class="block text-gray-700 font-medium mb-2">Email</label>
-                    <input type="text" class="form-input w-full" id="email" name="email">
-                </div>
-                <div class="mb-6">
-                    <label for="password" class="block text-gray-700 font-medium mb-2">Password</label>
-                    <input type="password" class="form-input w-full" id="password" name="password">
-                </div>
-                <div class="flex justify-between items-center mb-6">
-                    <a href="formLupaPassword.php" class="text-sm text-gray-500 hover:text-gray-700">Lupa Password</a>
-                    <a href="formSignUp.php" class="text-sm text-gray-500 hover:text-gray-700">Sign Up</a>
-                </div>
-                <button id="Login" type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">Login</button>
-            </form>
-        </div>
-    </body>
+<html class="h-full">
+<head>
+    <title>Login Siswa</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100 flex items-center justify-center h-full">
+    <div class="bg-white rounded-lg shadow-lg p-8 max-w-md w-full space-y-4">
+        <form method="post" action="loginProsesSiswa.php">
+            <div class="space-y-2">
+                <label for="email" class="font-medium text-gray-700">Email</label>
+                <input type="text" class="border border-gray-300 rounded-lg py-2 px-3 w-full" id="email" name="email">
+            </div>
+            <div class="space-y-2">
+                <label for="password" class="font-medium text-gray-700">Password</label>
+                <input type="password" class="border border-gray-300 rounded-lg py-2 px-3 w-full" id="password" name="password">
+            </div>
+            <div class="flex justify-between items-center">
+                <a href="formLupaPassword.php" class="text-blue-500">Lupa Password</a>
+                <a href="formSignUp.php" class="text-blue-500">Sign Up</a>
+            </div>
+            <button id="Login" type="submit" class="w-full py-2 px-4 bg-blue-500 text-white font-medium rounded-lg shadow-md hover:bg-blue-600 transition duration-200">Login</button>
+        </form>
+    </div>
+</body>
 </html>
