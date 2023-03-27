0adb9f8dfef2adb81ec22ab7a84fc32b0bb1f3da
Modbilas <238.juanjen@gmail.com>
2023-03-27 17:18:24 +0700

    update database

--- ProfileAdmin.php
+++ ProfileAdmin.php
@@ -113,34 +113,28 @@
 
 
     <div class="sticky-bottom">
-        <footer
-        
         <div class="fixed-bottom">
             <footer
                 class="text-center text-lg-start text-white"
                 style="background-color: #929fba"
                 >
             <div class="container p-4 pb-0">
-            <section class="">
-                <div class="row">
-                <div class="col-md-2 col-lg-3 col-xl-3 mx-auto mt-3">
-                    <h6 class="text-uppercase mb-1 font-weight-bold">
-                    SMP Juan Terro, Gading Serpong
-                    </h6>
-                
-                    
-                    <div>
-                      
-                    <p><i class="fas fa-envelope mr-3"></i> smpjuanterro@ac.id |   021-123456789</p>
+                <section class="">
+                    <div class="row">
+                        <div class="col-md-2 col-lg-3 col-xl-3 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-1 font-weight-bold">
+                            SMP Juan Terro, Gading Serpong
+                            </h6>
+                            <div>
+                                <p><i class="fas fa-envelope mr-3"></i> smpjuanterro@ac.id |   021-123456789</p>
+                            </div>
+                        </div>
                     </div>
-</div>
-                   </style>
-
-            </section>
-</div>
-
-        </footer>
+                </section>
+            </div>
+            </footer>
         </div>
+    </div>
     </body>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
