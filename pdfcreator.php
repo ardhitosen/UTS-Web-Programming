2c21f3e4d5d1ea482ac148ff158a031933bd4c52
finvalnnk14 <115550259+finvalnnk14@users.noreply.github.com>
2023-04-01 20:02:58 +0700

    tolong dong yg tulisan nama sama nis nya rapiin jd ke sebelah kanan foto

--- pdfcreator.php
+++ pdfcreator.php
@@ -1,45 +1,24 @@
 <?php
 session_start();
 require_once('vendor/fpdf.php');
-<<<<<<< Updated upstream
-// $dsn = "mysql:host=localhost;dbname=sekolah_lmao";
-// $db = new PDO($dsn, "root", "");
-=======
-/*$dsn = "mysql:host=localhost;dbname=sekolah_lmao";
-$db = new PDO($dsn, "root", "");
->>>>>>> Stashed changes
 
-// $id = $_SESSION['userid'];
 
-<<<<<<< Updated upstream
-// $sql = "SELECT Nama FROM siswa WHERE IDsiswa = {'$id'}";
-// $hasil = $db->query($sql);
-// $row = $hasil->fetch(PDO::FETCH_ASSOC);
-
-$nis = mt_rand(1000,9999);
-=======
-$sql = "SELECT Nama FROM siswa WHERE IDsiswa = {'$id'}";
-$hasil = $db->query($sql);
-$row = $hasil->fetch(PDO::FETCH_ASSOC);*/
->>>>>>> Stashed changes
 
 $nis = mt_rand(900,1000);
 $pdf = new FPDF('L','mm','Letter');
 $pdf->AddPage();
-<<<<<<< Updated upstream
-$pdf->Image('image/background.jpg',90,10,100);
-$pdf->SetFont('Arial','B',50);
+
+$pdf->Image('image/header.jpeg',50,10,200);
+$pdf->SetFont('Arial','B',20);
 //$pdf->Cell(10,160,"Nama: ".$row['Nama'],0);
-$pdf->Cell(10,160,"Nama: ",0); //buat tes
-$pdf->MultiCell(0,75,"");
-$pdf->Cell(10,110,"NIS: ".$nis,0);
-=======
-$pdf->Image('image/background.jpg',20,50,50);
+$pdf->Cell(100,100,"Nama: ",500); //buat tes
+$pdf->MultiCell(100,10,"");
+$pdf->Cell(100,100,"NIS: ".$nis,500);
+
+$pdf->Image('image/photopas.jpg',20,58,58);
 $pdf->SetFont('Arial','B',12);
-$pdf->Cell(10,160,"Nama: "/*.$row['Nama']*/,0);
-$pdf->MultiCell(0,10,"");
-$pdf->Cell(10,160,"NIS: ".$nis,0);
->>>>>>> Stashed changes
+$pdf->MultiCell(100,10,"");
+
 $pdf->Output();
 
 ?>     
